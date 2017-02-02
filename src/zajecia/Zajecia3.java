1024edac6e80283296f86617e5915692f5935fbe
unknown <ggozdziak@gmail.com>
2017-02-02 20:45:35 +0100

    Added productOfPositive method

--- src/zajecia/Zajecia3.java
+++ src/zajecia/Zajecia3.java
@@ -6,23 +6,35 @@ package zajecia;
 public class Zajecia3 {
 
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7,8,9,12,13,14,16};
+        int[] array = {1, -2, 3, -4, 5};
         int sum = sumOf(array);
-        System.out.println("Suma wszystkich el. tablicy: " + sum);
+        //System.out.println("Suma wszystkich el. tablicy: " + sum);
+        int product = productOfPositive(array);
+        System.out.println("Iloczyn dodatnich el. tablicy: " + product);
     }
 
-    public static int sumOf(int[] array){
+    public static int productOfPositive(int[] array) {
+        int product = 1;
+        for (int i = 0; i < array.length; i++) {
+            if (isPositive(array[i])) {
+                product *= array[i];
+            }
+        }
+        return product;
+    }
+
+    public static int sumOf(int[] array) {
         int sum = 0;
-        for(int i = 0 ; i < array.length ; i++){
-            if(isPositive(array[i])){
+        for (int i = 0; i < array.length; i++) {
+            if (isPositive(array[i])) {
                 sum += array[i];
             }
         }
         return sum;
     }
 
-    public static boolean isPositive(int data){
-        if(data > 0){
+    public static boolean isPositive(int data) {
+        if (data > 0) {
             return true;
         } else {
             return false;
